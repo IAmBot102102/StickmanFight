78a9e257601ce18f9c8f4d21da751a79f3c5e682
LilKalebFTW2 <54673099+LilKalebFTW2@users.noreply.github.com>
2020-03-11 10:12:31 -0400

    Update websocket.js

--- Client/websocket.js
+++ Client/websocket.js
@@ -1,9 +1,7 @@
 let websocket = new WebSocket("wss://10.37.110.140");
-websocket.onopen = function(event)
-{
+websocket.onopen = function(event){
 
 }
-websocket.onmessage = function(event)
-{
+websocket.onmessage = function(event){
     console.log(event.data);
 }
