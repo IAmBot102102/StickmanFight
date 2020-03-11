46248e74cefa39806ef5a521a40a4ad246b18e38
IAmBot102102 <BotNumber102102@gmail.com>
2020-03-11 10:41:09 -0400

    added jQuery

--- Client/websocket.js
+++ Client/websocket.js
@@ -1,4 +1,4 @@
-let websocket = new WebSocket("wss://10.37.110.140:3030");
+let websocket = new WebSocket("ws://10.37.110.240:3030");
 websocket.onopen = function(event){
     websocket.send("dasfdsafdsa");
     console.log("dasfdsafdsa");
