6585d276b647349a820375e8d60dd444f1cba416
IAmBot102102 <BotNumber102102@gmail.com>
2020-03-11 10:21:52 -0400

    Currently Fixing the websocket Connection

--- Client/websocket.js
+++ Client/websocket.js
@@ -1,7 +1,8 @@
-let websocket = new WebSocket("wss://10.37.110.140");
+let websocket = new WebSocket("wss://10.37.110.140:3030");
 websocket.onopen = function(event){
-
-}
+    websocket.send("dasfdsafdsa");
+    console.log("dasfdsafdsa");
+};
 websocket.onmessage = function(event){
     console.log(event.data);
-}
+};
